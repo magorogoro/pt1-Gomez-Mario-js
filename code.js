72b19a6a6c63d61dfe7f70b969b8f1854d3967c2
magorogoro <123159683+magorogoro@users.noreply.github.com>
2023-05-10 09:49:28 +0000

    lñ´

--- code.js
+++ code.js
@@ -1,57 +1,35 @@
+
+
+/* Modo Oscuro Menu Navegacion */
 function myFunction() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
 
 
-const canvas = document.getElementById('spinner');
-const ctx = canvas.getContext('2d');
-const names = ['Nombre 1', 'Nombre 2', 'Nombre 3', 'Nombre 4', 'Nombre 5'];
-const colors = ['#007bff', '#28a745', '#dc3545', '#ffc107', '#17a2b8'];
-const spinButton = document.getElementById('spin-button');
+// Obtener elementos del reloj
+const hourElement = document.getElementById('hour');
+const minuteElement = document.getElementById('minute');
+const secondElement = document.getElementById('second');
 
-let spinning = false;
-let angle = 0;
-let nameIndex = -1;
+// Actualizar el reloj
+function updateClock() {
+  // Obtener la hora actual
+  const currentTime = new Date();
+  let hours = currentTime.getHours();
+  let minutes = currentTime.getMinutes();
+  let seconds = currentTime.getSeconds();
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  ctx.fillStyle = colors[nameIndex];
-  ctx.beginPath();
-  ctx.moveTo(canvas.width / 2, canvas.height / 2);
-  ctx.arc(canvas.width / 2, canvas.height / 2, 150, angle, angle + Math.PI * 2 / names.length, false);
-  ctx.lineTo(canvas.width / 2, canvas.height / 2);
-  ctx.fill();
-  for (let i = 0; i < names.length; i++) {
-    const textAngle = angle + Math.PI * 2 / names.length * (i + 0.5);
-    ctx.save();
-    ctx.translate(canvas.width / 2, canvas.height / 2);
-    ctx.rotate(textAngle);
-    ctx.fillStyle = 'white';
-    ctx.font = '24px Arial';
-    ctx.fillText(names[i], 70, 0);
-    ctx.restore();
-  }
-  angle += Math.PI / 64;
-  if (angle >= Math.PI * 2) {
-    angle -= Math.PI * 2;
-    spinning = false;
-    alert(`El nombre seleccionado es: ${names[nameIndex]}`);
-  }
-  if (spinning) {
-    requestAnimationFrame(draw);
-  }
+  // Agregar un cero inicial a los números menores de 10
+  hours = (hours < 10) ? "0" + hours : hours;
+  minutes = (minutes < 10) ? "0" + minutes : minutes;
+  seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  // Actualizar el contenido HTML de los elementos del reloj
+  hourElement.textContent = hours;
+  minuteElement.textContent = minutes;
+  secondElement.textContent = seconds;
 }
 
-spinButton.addEventListener('click', () => {
-  if (!spinning) {
-    spinning = true;
-    nameIndex = Math.floor(Math.random() * names.length);
-    spinButton.disabled = true;
-    draw();
-    setTimeout(() => {
-      spinButton.disabled = false;
-    }, 2000);
-  }
-});
-
+// Actualizar el reloj cada segundo
+setInterval(updateClock, 1000);
