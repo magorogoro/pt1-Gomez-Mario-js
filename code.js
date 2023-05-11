ed0b7fa4427c570a2a121068e215372c5f5c415c
magorogoro <123159683+magorogoro@users.noreply.github.com>
2023-05-11 19:03:58 +0000

    fgg

--- code.js
+++ code.js
@@ -44,6 +44,7 @@ let secondElement = document.getElementById('second');
 setInterval(updateClock, 1000);
 
 
+
 const sectors = [
   {color:"#f82", label:"Juan"},
   {color:"#0bf", label:"Lucas"},
@@ -55,77 +56,61 @@ const sectors = [
   {color:"#bf0", label:"Antonio"},
 ];
 
-// Generate random float in range min-max:
-const rand = (m, M) => Math.random() * (M - m) + m;
-
-const tot = sectors.length;
 const elSpin = document.querySelector("#spin");
-const ctx = document.querySelector("#wheel").getContext`2d`;
+const ctx = document.querySelector("#wheel").getContext("2d");
 const dia = ctx.canvas.width;
 const rad = dia / 2;
-const PI = Math.PI;
-const TAU = 2 * PI;
+const TAU = 2 * Math.PI;
+const tot = sectors.length;
 const arc = TAU / tot;
-const friction = 0.991;  // 0.995=soft, 0.99=mid, 0.98=hard
-const angVelMin = 0.002; // Below that number will be treated as a stop
-let angVelMax = 0; // Random ang.vel. to accelerate to 
-let angVel = 0;    // Current angular velocity
-let ang = 0;       // Angle rotation in radians
+const friction = 0.991;
+const angVelMin = 0.002;
+let angVelMax = 0;
+let angVel = 0;
+let ang = 0;
 let isSpinning = false;
 let isAccelerating = false;
-let animFrame = null; // Engine's requestAnimationFrame
+let animFrame = null;
 
-//* Get index of current sector */
 const getIndex = () => Math.floor(tot - ang / TAU * tot) % tot;
 
-//* Draw sectors and prizes texts to canvas */
 const drawSector = (sector, i) => {
-  const ang = arc * i;
+  const angle = arc * i;
   ctx.save();
-  // COLOR
   ctx.beginPath();
   ctx.fillStyle = sector.color;
   ctx.moveTo(rad, rad);
-  ctx.arc(rad, rad, rad, ang, ang + arc);
+  ctx.arc(rad, rad, rad, angle, angle + arc);
   ctx.lineTo(rad, rad);
   ctx.fill();
-  // TEXT
   ctx.translate(rad, rad);
-  ctx.rotate(ang + arc / 2);
+  ctx.rotate(angle + arc / 2);
   ctx.textAlign = "right";
   ctx.fillStyle = "#fff";
   ctx.font = "bold 24px sans-serif";
   ctx.fillText(sector.label, rad - 10, 10);
-  //
   ctx.restore();
 };
 
-//* CSS rotate CANVAS Element */
 const rotate = () => {
   const sector = sectors[getIndex()];
-  ctx.canvas.style.transform = `rotate(${ang - PI / 2}rad)`;
+  ctx.canvas.style.transform = `rotate(${ang - Math.PI / 2}rad)`;
   elSpin.textContent = !angVel ? "SPIN" : sector.label;
   elSpin.style.background = sector.color;
 };
 
 const frame = () => {
-
   if (!isSpinning) return;
 
   if (angVel >= angVelMax) isAccelerating = false;
 
-  // Accelerate
   if (isAccelerating) {
-    angVel ||= angVelMin; // Initial velocity kick
-    angVel *= 1.1; // Accelerate
-  }
-  
-  // Decelerate
-  else {
+    angVel ||= angVelMin;
+    angVel *= 1.1;
+  } else {
     isAccelerating = false;
-    angVel *= friction; // Decelerate by friction  
+    angVel *= friction;
 
-    // SPIN END:
     if (angVel < angVelMin) {
       isSpinning = false;
       angVel = 0;
@@ -133,30 +118,25 @@ const frame = () => {
     }
   }
 
-  ang += angVel; // Update angle
-  ang %= TAU;    // Normalize angle
-  rotate();      // CSS rotate!
+  ang += angVel;
+  ang %= TAU;
+  rotate();
 };
 
 const engine = () => {
   frame();
-  animFrame = requestAnimationFrame(engine)
+  animFrame = requestAnimationFrame(engine);
 };
 
 elSpin.addEventListener("click", () => {
   if (isSpinning) return;
   isSpinning = true;
   isAccelerating = true;
-  angVelMax = rand(0.25, 0.40);
-  engine(); // Start engine!
+  angVelMax = Math.random() * (0.4 - 0.25) + 0.25;
+  engine();
 });
 
-// INIT!
 sectors.forEach(drawSector);
-rotate(); // Initial rotation
-}
+rotate();
 
-
-
-
-
+ }
